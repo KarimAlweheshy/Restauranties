f7d04be6f1c031f9b16ceba4b0e03c7d1b0a1cd2
Karim <karim.alweheshy@gmail.com>
2020-11-03 15:12:49 +0100

    Add Callable functions to change user claims

--- firebase_cloud_functions/functions/index.js
+++ firebase_cloud_functions/functions/index.js
@@ -1,9 +1,39 @@
+const { integrify } = require('integrify');
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+integrify({ config: { functions, db } });
+
+// Rules will be loaded from "integrify.rules.js"
+module.exports = integrify();
+
+module.exports.becomeOwner = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' + 'while authenticated.');
+  }
+  return admin.auth().setCustomUserClaims(context.auth.uid, {owner: true}).then(() => {
+    return { message: 'User is now an owner' };
+  });
+});
+
+module.exports.becomeRater = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' + 'while authenticated.');
+  }
+  return admin.auth().setCustomUserClaims(context.auth.uid, {}).then(() => {
+    return { message: 'User is now a rater' };
+  });
+});
+
+module.exports.becomeAdmin = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' + 'while authenticated.');
+  }
+  return admin.auth().setCustomUserClaims(context.auth.uid, {admin: true}).then(() => {
+    return { message: 'User is now an admin' };
+  });
+});
