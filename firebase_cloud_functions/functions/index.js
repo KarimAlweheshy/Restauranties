0a603d86bdca217aa1ae0432a98ed83c466aad40
Karim <karim.alweheshy@gmail.com>
2020-11-04 14:32:30 +0100

    Add functions to retrieve owner restaurants and create new ones

--- firebase_cloud_functions/functions/index.js
+++ firebase_cloud_functions/functions/index.js
@@ -68,6 +68,38 @@ module.exports.deleteUser = functions.https.onCall(async (data, context) => {
   }
 });
 
+module.exports.myRestaurants = functions.https.onCall(async (data, context) => {
+  verifyAuth(context)
+  await verifyIsRestaurantOwnerUser(admin, context.auth.uid)
+  const restaurantsCollection = db.collection("restaurants")
+  return await restaurantsCollection.where('ownerID', '==', context.auth.uid).get()
+});
+
+module.exports.addRestaurant = functions.https.onCall(async (data, context) => {
+  verifyAuth(context)
+  await verifyIsRestaurantOwnerUser(admin, context.auth.uid)
+  verifyRestaurantKeysAndValues(data)
+  const restaurantsCollection = db.collection("restaurants")
+
+  const restaurantWithSameName = await restaurantsCollection.where('name', '==', data.name).get()
+  if (restaurantWithSameName) {
+    throw new functions.https.HttpsError('failed-precondition', 'Owner already has restaurant with same name');  
+  }
+  
+  let restaurant = {
+    name: data.name,
+    phone: data.phoneNumber,
+    imageURL: data.imageURL,
+    categories: data.categories,
+    ownerID: context.auth.uid,
+    totalRatings: 0,
+    averageRating: 0,
+    creationDate: admin.firestore.Timestamp.fromDate(new Date()),
+    modificationDate: admin.firestore.Timestamp.fromDate(new Date()),
+  }
+  return await restaurantsCollection.add(restaurant)
+});
+
 async function findUser(admin, uid) {
   try {
     return await admin.auth().getUser(uid);
@@ -85,6 +117,13 @@ function verifyAuth(context) {
 async function verifyIsAdminUser(admin, uid) {
   const user = await findUser(admin, uid)
   if (!isAdminUser(user)) {
+    throw new functions.https.HttpsError('failed-precondition', 'Only Restaurant Owner is allowed to access such calls');
+  }
+}
+
+async function verifyIsRestaurantOwnerUser(admin, uid) {
+  const user = await findUser(admin, uid)
+  if (!isRestaurantOwnerUser(user)) {
     throw new functions.https.HttpsError('failed-precondition', 'Only Admin is allowed to access such calls');
   }
 }
@@ -92,4 +131,13 @@ async function verifyIsAdminUser(admin, uid) {
 function isAdminUser(user) {
   const claims = user.customClaims;
   return claims && claims['admin'] === true
+}
+
+function isRestaurantOwnerUser(user) {
+  const claims = user.customClaims;
+  return claims && claims['owner'] === true
+}
+
+function verifyRestaurantKeysAndValues(restaurant) {
+  return restaurant.name && restaurant.name.length > 2 
 }
